ffcfb30d3e1e656ca5b1c777c668b30878ac7928
Rafael Chung <rafaelchung28@hotmail.com>
2024-10-12 14:00:06 -0500

    Subiendo be

--- fe/src/app/cuentas/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Cuentas() {
-  return (
-    <div>Cuentas</div>
-  )
-}
-
-export default Cuentas
--- fe/src/app/ventas/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Ventas() {
-  return (
-    <div>Ventas</div>
-  )
-}
-
-export default Ventas
