29b1addccc8b128df4efc202ca2e50e4f4dcbeec
Rafael Chung <rafaelchung28@hotmail.com>
2024-10-12 14:26:34 -0500

    eliminacion de be

--- src/cuentas/cuentas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CuentasService } from './cuentas.service';
-import { CuentasController } from './cuentas.controller';
-
-@Module({
-  controllers: [CuentasController],
-  providers: [CuentasService],
-})
-export class CuentasModule {}
